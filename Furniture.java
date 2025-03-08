b83ea0459806162c93ef9ae2fcce890bb914e289
reganYK <regan.yazdankhahkenari@student.tdsb.on.ca>
2025-03-08 01:21:09 -0500

    Create Furniture.java

---
+++ Furniture.java
@@ -0,0 +1,34 @@
+abstract class Furniture{
+	String colour;
+
+	abstract void Function();
+
+	abstract void Location();
+}
+
+class Bed extends Furniture{
+	void Function(){
+		System.out.println("Bed is used to sleep on.");
+	}
+	void Location(){
+		System.out.println("Bed is located in the bedroom.");
+	}
+}
+
+class Couch extends Furniture{
+	void Function(){
+		System.out.println("Couch is used to relax on.");
+	}
+	void Location(){
+		System.out.println("Couch is located in the living room.");
+	}
+}
+
+class Dishwasher extends Furniture{
+	void Function(){
+		System.out.println("Table is used to gather family members around to eat a meal.");
+	}
+	void Location(){
+		System.out.println("Table is located in the dining room.");
+	}
+}
